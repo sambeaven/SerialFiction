7690f68cdff44f2ef1c0191bb49db769b1ba0c85
sambeaven <darkhawke@gmail.com>
2015-05-09 17:09:32 +0100

    Initial commit

---
+++ SerialFiction/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SerialFiction.Startup))]
+namespace SerialFiction
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
